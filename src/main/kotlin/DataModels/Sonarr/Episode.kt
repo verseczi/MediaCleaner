27d7c8774d79293a1a2fbf0883093a78a02002c8
Tamás Verseczi <verseczi@gmail.com>
2018-07-08 18:25:13 +0200

    Sonarr Data model cleared A bug causing crashes when deleting fixed. (sonarr)

--- src/main/kotlin/DataModels/Sonarr/Episode.kt
+++ src/main/kotlin/DataModels/Sonarr/Episode.kt
@@ -1,20 +1,11 @@
 package com.mediacleaner.DataModels.Sonarr
 
 data class Episode(
-        val seriesId: Int,
         val episodeFileId: Int,
-        val seasonNumber: Int,
-        val episodeNumber: Int,
-        val title: String,
         val hasFile: Boolean,
-        val id: Int,
         val episodeFile: EpisodeFile? = null
 ) {
     data class EpisodeFile(
-            val seriesId: Int,
-            val seasonNumber: Int,
-            val relativePath: String,
-            val path: String,
-            val id: Int
+            val path: String = ""
     )
 }
